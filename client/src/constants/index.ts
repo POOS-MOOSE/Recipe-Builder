8de827ec25606517d692cb1098b2220643999ddf
franklaterza <franklaterza@gmail.com>
2025-04-15 18:01:42 -0400

    prebuild3

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,4 +1,4 @@
 // api url (where your server is hosted at)
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080'
+const BACKEND_URL = 'http://fullybaked.me'|| 'http://localhost:8080'
 
 export { BACKEND_URL }
