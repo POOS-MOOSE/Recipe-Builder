56906ad5fb1ef317db3a8404fd40df1e9ce118ff
franklaterza <franklaterza@gmail.com>
2025-04-16 15:41:02 +0000

    deploy fix

--- bitnami-deploy.sh
+++ bitnami-deploy.sh
@@ -3,16 +3,16 @@
 SESSION="server"
 
 tmux has-session -t $SESSION 2>/dev/null
-if [ $? != 0 ]; then
-    tmux new-session -d -s $SESSION
+if [ $? -eq 0 ]; then
+	    tmux kill-session -t $SESSION
 fi
 
+tmux new-session -d -s $SESSION
 tmux send-keys -t $SESSION:0 'cd server && npm run dev' C-m
 
+rm -r ~/htdocs/*
 cp -r client/build/* ~/htdocs/
-
 cp vhost.conf /opt/bitnami/apache/conf/vhosts/vhost.conf
-
 sudo /opt/bitnami/ctlscript.sh restart apache
 
 
