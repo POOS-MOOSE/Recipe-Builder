c31d66c6bd2d2b37cffa9822022e7844d2868f1e
FrankLaterza <franklaterza@gmail.com>
2025-04-16 11:18:36 -0400

    change server link

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,4 +1,4 @@
 // api url (where your server is hosted at)
-const BACKEND_URL = 'https://fullybaked.me'|| 'http://localhost:8080'
+const BACKEND_URL = 'http://server.fullybaked.me'|| 'http://localhost:8080'
 
 export { BACKEND_URL }
