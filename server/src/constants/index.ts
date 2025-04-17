9f53c8ecc3aaa066c10f670376e445fff00ee723
franklaterza <franklaterza@gmail.com>
2025-04-17 10:59:34 -0400

    meal plan edit page done

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,4 +1,14 @@
-const ORIGIN = '*'
+// Set NODE_ENV based on how we're running the server
+// When run with 'npm run dev', it will be in development mode
+// When run with 'npm run build && npm start', it will be in production mode
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const isProd = NODE_ENV === 'production'
+
+// Configure CORS settings based on environment
+const ORIGIN = isProd 
+  ? 'http://server.fullybaked.me' // Production domain
+  : '*' // Allow all origins in development
+
 const PORT = process.env.PORT || 8080
 
 // For "MongoDB Atlas": edit MONGO_URI in -> .env file
@@ -13,4 +23,17 @@ const MONGO_OPTIONS = {
 
 const JWT_SECRET = process.env.JWT_SECRET || 'unsafe_secret'
 
-export { ORIGIN, PORT, MONGO_URI, MONGO_OPTIONS, JWT_SECRET }
+const BASE_URL = isProd
+  ? 'http://server.fullybaked.me' // Production domain
+  : `http://localhost:${PORT}` // Development URL
+
+export { 
+  NODE_ENV, 
+  isProd, 
+  ORIGIN, 
+  PORT, 
+  MONGO_URI, 
+  MONGO_OPTIONS, 
+  JWT_SECRET,
+  BASE_URL 
+}
